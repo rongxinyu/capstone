62667852ede1893dc81121063ccc1af1397fe287
weiyialanchen <weiyi.alan.chen@gmail.com>
2015-11-04 02:08:50 -0500

    python version

--- python-version/main.py
+++ python-version/main.py
@@ -1,5 +1,9 @@
 import mibian
 from numpy import *
+import matplotlib.pyplot as pyplot
+
+def myConvolve(x, y, length):
+    return sum(x[:length] * y[:length][::-1])
 
 def bstar(k, alpha):
     return ((k**(alpha+1) - (k-1)**(alpha+1)) / (alpha+1)) ** (1/alpha)
@@ -14,10 +18,10 @@ def hybridScheme(params):
     def covMatrix(n, kappa):
         sigma = zeros((kappa+1, kappa+1))
         sigma[0][0] = 1./n
-        for j in xrange(1, kappa+1):
+        for j in range(1, kappa+1):
             sigma[0][j] = sigma[j][0] = (j**(alpha+1) - (j-1)**(alpha+1)) / ((alpha+1)* n**(alpha+1))
-        for j in xrange(1, kappa+1):
-            for k in xrange(1, kappa+1):
+        for j in range(1, kappa+1):
+            for k in range(1, kappa+1):
                 if j == k:
                     sigma[j][k] = (j**(2*alpha+1) - (j-1)**(2*alpha+1)) / ((2*alpha+1)*n**(2*alpha+1))
             else:
@@ -25,25 +29,25 @@ def hybridScheme(params):
         return sigma 
 
     def Simulation(n, kappa, T):
-        W = array([random.multivariate_normal(mean=[0]*(kappa+1), cov=covMatrix(n, kappa)) for i in xrange(int(n*T))])
+        W = array([random.multivariate_normal(mean=[0]*(kappa+1), cov=covMatrix(n, kappa)) for i in range(int(n*T))])
         Wperp = random.normal(scale=sqrt(1./n), size=int(n*T))
         Z = rho * W[:,0] + sqrt(1-rho**2) * Wperp
 
-        Gamma = array([(bstar(x+1, alpha)/n) ** alpha for x in xrange(int(n*T))])
-        Gamma[:kappa+1] = 0
-        Y2 = convolve(Gamma, W[:,0])[:100] # size 199
+        Gamma = array([(bstar(x+1, alpha)/n) ** alpha for x in range(int(n*T))])
+        Gamma[:kappa] = 0
+        Y2 = array([myConvolve(Gamma, W[:,0], x+1) for x in range(int(n*T))])
         Y1 = zeros(int(n*T))
-        for i in xrange(int(n*T)):
-            Y1[i] = 0
+        for i in range(int(n*T)):
+            Y1[i] = 0.
             for k in range(min(i, kappa-1)):
                 Y1[i] = Y1[i] + W[i+1-k][k+1]
         Y = Y1 + Y2
-        v = xi * exp(eta * sqrt(2*alpha+1)*Y - eta**2/2 * array([(x/n)**(2*alpha+1) for i in xrange(int(n*T))]))
+        v = xi * exp(eta * sqrt(2*alpha+1)*Y - eta**2/2 * array([(x/n)**(2*alpha+1) for x in range(int(n*T))]))
         S = S0 * exp(sum(v**0.5 * Z) - 0.5*sum(v)/n)
         return S
 
     def MC(N, n, kappa, T):
-        return [Simulation(n, kappa, T) for i in xrange(N)]
+        return array([Simulation(n, kappa, T) for i in range(N)])
 
     return MC
 
@@ -52,9 +56,11 @@ def impvol(k, st, T):
     return BSImpliedVolCall(1, exp(k), T, 0, mean(payoff))
 
 def main():
-    params = (1., 0.235**2, 1.9, -.43, -.9)
-    finalPrices = hybridScheme(params)(10000, 100, 1, 1.)
-    print finalPrices
+    params = (1., 0.235**2, 1.9, -.43, -0.)
+    finalPrices = hybridScheme(params)(100, 100, 1, 1.)
+    strikes = array(range(-100,100)) / 100.
+    vols = array([impvol(k, finalPrices, 1,) for k in strikes])
+    pyplot.plot(strikes, vols)
 
 if __name__ == '__main__':
     main()
