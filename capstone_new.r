736732159f260c56aa5815cf163dd811fe5af945
rongxinyu <yrx1991@gmail.com>
2015-11-07 08:58:26 -0500

    use the new file

--- capstone_new.r
+++ capstone_new.r
@@ -1,15 +1,9 @@
 rm(list=ls())
 library(MASS)
 library(gsl)
-<<<<<<< Updated upstream
 library(ggplot2)
 Myconvolve <- function(x, y, length){
   return(sum(x[1:length] * rev(y[1:length])))
-=======
-
-myConvolve <- function(x, y, length){
-    return(sum(x[1:length] * rev(y[1:length])))
->>>>>>> Stashed changes
 }
 
 bstar <- function(k, alpha){
@@ -40,7 +34,7 @@ BSImpliedVolCall <- function(S0, K, T, r, C)
 MyImpliedVolCall <- function(S0, K, T, r, C){
   sigmaL <- 1e-3
   sigmaH <- 10
-  while(sigmaH-sigmaL > 1e-5){
+  while(sigmaH-sigmaL > 1e-10){
     sigma <- (sigmaL + sigmaH)/2
     if(BSFormula(S0, K, T, r, sigma)==C) {break}
     else if(BSFormula(S0, K, T, r, sigma)>C) {sigmaH <- sigma}
@@ -100,121 +94,48 @@ hybridScheme <- function(params){
         }
       }
     }
-<<<<<<< Updated upstream
     #print(sigma)
     return(sigma)
   }
   
   Simulation <- function(n, kappa, T){
     W <- mvrnorm(floor(n*T), mu=rep(0, kappa+1), Sigma=covMatrix(n, kappa))
-    #print(W)
     Wperp <- rnorm(floor(n*T), sd=sqrt(1/n))
     Z <- rho * W[,1] + sqrt(1-rho*rho)*Wperp
-    #print(n)
     Gamma <- sapply(seq(1:floor(n*T)), function(x){(bstar(x, alpha)/n)^alpha}) 
     Gamma[1:kappa] <- 0
     Y2 <- sapply(seq(1,floor(n*T)), function(x){Myconvolve(Gamma, W[,1], x)})
     Y1 <- rep(0, floor(n*T))
-    #print(length(Y1))
-    #print(length(Y2))
+    
     for(i in 1:floor(n*T)){
       Y1[i] <-0
       for (k in 1:min(i,kappa)){
         Y1[i] = Y1[i] + W[i+1-k,k+1]
       }
-=======
-    
-    Simulation <- function(n, kappa, T){
-       W <- mvrnorm(floor(n*T), mu=rep(0, kappa+1), Sigma=covMatrix(n, kappa))
-       for(i in 1:(kappa+1)) {W[,i] = W[,i] - mean(W[,i])}
-       #print(W)
-       Wperp <- rnorm(floor(n*T), sd=sqrt(1/n))
-       Wperp <- Wperp - mean(Wperp)
-       Wperp <- Wperp/sd(Wperp)*1/n
-       Z <- rho * W[,1] + sqrt(1-rho*rho)*Wperp
-       
-       Gamma <- sapply(seq(1:floor(n*T)), function(x){(bstar(x, alpha)/n)^alpha}) 
-       Gamma[1:kappa] <- 0
-       Y2 <- sapply(seq(1:floor(n*T)), function(x){myConvolve(Gamma, W[,1], x)})
-       Y1 <- rep(0, floor(n*T))
-       for(i in 1:floor(n*T)){
-         Y1[i] <-0
-         for (k in 1:min(i,kappa)){
-         Y1[i] = Y1[i] + W[i+1-k,k+1]
-         }
-       }
-       Y <- Y1+Y2 ## The simulated series of main interst
-       v <- xi*exp(eta*sqrt(2*alpha+1)*Y - eta*eta/2*sapply(seq(1:floor(n*T)),function(x){(x/n)^(2*alpha+1)}))
-       #print(summary(v))
-       #return(v[floor(n*T)])
-       S <- S0 * exp(sum(v^0.5*Z) - 1/2*sum(v)/n)
-       #S <- prod(v^0.5*Z+1)
-       #print(S)
-       return(S)
-       #return(min(v))
-      #return(1)
->>>>>>> Stashed changes
     }
     Y <- Y1+Y2 ## The simulated series of main interst
     v <- xi*exp(eta*sqrt(2*alpha+1)*Y - eta*eta/2*sapply(seq(1:floor(n*T)),function(x){(x/n)^(2*alpha+1)}))
-    #print(summary(v))
-    #return(v[floor(n*T)])
-    #S <- S0 * exp(sum(v^0.5*Z) - 1/2*sum(v)/n)
-    #print(length(v))
+    v <- c(xi, v[1:length(v)-1])
     S <- S0 * exp(sum(v^0.5*Z) - 1/2*sum(v)/n)
-    #print(S)
-    #plot(cumsum(v^0.5*Z))
-    #plot(Z, W[,1])
-    #plot(v^0.5)
-    #print(cumsum)
-    #data = data.frame(seq(1,length(S)),S)
-    #print(qplot(data.frame(seq(1,length(S)),S)))
-    #print(length(S))
-    #plot(S)
     return(S)
-    #return(1)
-    }
-    
-    MC <- function(N, n, kappa, T){
-      return(sapply(rep(T, N), function(x){Simulation(n, kappa, x)}))
-    }
-    return(MC)
   }
   
-  impvol <- function(k, st, T){
-    payoff <- (st > exp(k)) * (st - exp(k))
-    #print(summary(payoff))
-    return(BSImpliedVolCall(1, exp(k), T, 0, mean(payoff)))
-<<<<<<< Updated upstream
+  MC <- function(N, n, kappa, T){
+    return(sapply(rep(T, N), function(x){Simulation(n, kappa, x)}))
   }
-  params <- list(S0=1, xi=0.235^2, eta=1.9, alpha=-0.43, rho=-0.2)
-  finalPrices <- hybridScheme(params)(100000, 100, 1, 1)
-  #summary(finalPrices)
-  par(mfrow=c(2,1))
-  hist(finalPrices)
-  
-  vol <- function(k){sapply(k, function(x){impvol(x, finalPrices, 1)})}
-  payoff <- function(k){sapply(k, function(x){mean((finalPrices > exp(x)) * (finalPrices - exp(x)))})}
-  curve(vol(x), from=-0.5, to=0.5, xlab="Log strike", ylab="Implied Vol")
-  #BSprice <- function(k){sapply(k, function(x){BSFormula(1, exp(k), 1, 0, vol(k))})}
-  #curve(payoff(x), from=-0.5, to=0.5)
-  #curve(BSprice(x), from= -0.5, to =0.5, add=T, )
-  k <- seq(-0.5, 0.5, 0.1)
-  for(i in 1:10){
-    print(k[i])
-    print(payoff(k[i]))
-    print(BSFormula(1, exp(k[i]), 1, 0, vol(k[i])))
-    print("-----")
-  }
-  
-=======
+  return(MC)
 }
-params <- list(S0=1, xi=0.235^2, eta=1.9, alpha=-0.43, rho=-0.1)
-finalPrices <- hybridScheme(params)(1000, 100, 1, 1)
-par(mfrow=c(2,1))
-hist(finalPrices)
-summary(finalPrices)
-vol <- function(k){sapply(k, function(x){impvol(x, finalPrices, 1)})}
-curve(vol(x), from=-0.5, to=0.5)
 
->>>>>>> Stashed changes
+impvol <- function(k, st, T){
+  payoff <- (st > exp(k)) * (st - exp(k))
+  return(MyImpliedVolCall(1, exp(k), T, 0, mean(payoff)))
+}
+params <- list(S0=1, xi=0.235^2, eta=1.9, alpha=-0.43, rho=-0.9)
+finalPrices <- hybridScheme(params)(10000, 2000, 1, 0.041)
+summary(finalPrices)
+
+vol <- function(k){sapply(k, function(x){impvol(x, finalPrices, floor(n*T)/n)})}
+payoff <- function(k){sapply(k, function(x){mean((finalPrices > exp(x)) * (finalPrices - exp(x)))})}
+curve(vol(x), from=-0.5, to=0.5, xlab="Log strike", ylab="Implied Vol")
+
+
